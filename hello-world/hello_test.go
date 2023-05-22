faea90e92908fd4031203f6179effccc1a38ec57
t3yamoto <3yamoto.dev@gmail.com>
2023-05-22 20:59:08 +0900

    Refactor hello-world

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,10 +3,31 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	assertCorrectMessage := func(t testing.TB, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying 'Hello, world' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Adam", "French")
+		want := "Bonjour, Adam"
+		assertCorrectMessage(t, got, want)
+	})
 }
