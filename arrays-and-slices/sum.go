bb0e99e739da47c6d0f6b3a69eae26d5663e7a50
t3yamoto <3yamoto.dev@gmail.com>
2023-05-27 16:28:06 +0900

    sum

---
+++ arrays-and-slices/sum.go
@@ -0,0 +1,27 @@
+package sum
+
+func Sum(numbers []int) (sum int) {
+	for _, n := range numbers {
+		sum += n
+	}
+	return
+}
+
+func SumAll(numbersToSum ...[]int) (sums []int) {
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numberToSUm ...[]int) (sums []int) {
+	for _, numbers := range numberToSUm {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := numbers[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return
+}
